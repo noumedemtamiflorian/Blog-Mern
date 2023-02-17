883d5ca4b672e2cfc9d81998f1a00b4dcb8bb668
noumedem tami florian <noumedemtamiflorian@gmail.com>
2023-02-17 22:22:44 +0100

    Création du modèle « Comments » pour gérer les commentaires

--- backend/models/article.model.js
+++ backend/models/article.model.js
@@ -1,15 +1,22 @@
-// Import the mongoose module
+// Importer le module mongoose pour créer le schéma
 const mongoose = require("mongoose");
 
-// Create a Schema for an Article
+// Créer le schéma pour les articles
 const Schema = mongoose.Schema;
 const ArticleSchema = new Schema({
+    // Titre : Requis (String)
     title: { type: String, required: true },
+    // Contenu : Requis (String)
     content: { type: String, required: true },
+    // Description : Requis (String)
     description: { type: String, required: true },
+    // Image : Requis (String)
     image: { type: String, required: true },
+    // Catégorie : Requis (ObjectId, référence à la collection Category)
     category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+    // Commentaires : Optionnel (Array de ObjectId, référence à la collection Comment)
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
 });
 
-// Export the Article Schema
+// Exporter le modèle
 module.exports = mongoose.model("Article", ArticleSchema);
---
+++ backend/models/comment.model.js
@@ -0,0 +1,14 @@
+// Importer le module de mongoose
+const mongoose = require("mongoose");
+
+// Créer un schéma pour les commentaires
+const Schema = mongoose.Schema;
+const CommentSchema = new Schema({
+    // Contenu du commentaire
+    content: { type: String, required: true },
+    // Référence à l'article auquel le commentaire est lié
+    article: { type: Schema.Types.ObjectId, ref: "Article", required: true },
+});
+
+// Exporter le modèle de commentaire
+module.exports = mongoose.model("Comment", CommentSchema);
