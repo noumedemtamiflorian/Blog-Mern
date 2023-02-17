e6036fd9bd9cbba53e8c12dd06a3f3ee9a7dab93
noumedem tami florian <noumedemtamiflorian@gmail.com>
2023-02-17 22:22:44 +0100

    Création du modèle « Comments » pour gérer les commentaires

--- backend/models/article.model.js
+++ backend/models/article.model.js
@@ -1,11 +1,20 @@
+// Importer le module mongoose pour créer le schéma
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
+// Créer le schéma
+const Schema = mongoose.Schema;
 const ArticleSchema = new Schema({
+    // Ajouter un titre à l'article, ce champ est requis
     title: { type: String, required: true },
+    // Ajouter le contenu de l'article, ce champ est requis
     content: { type: String, required: true },
+    // Ajouter une description de l'article, ce champ est requis
     description: { type: String, required: true },
+    // Ajouter une image à l'article, ce champ est requis
     image: { type: String, required: true },
+    // Ajouter des commentaires à l'article
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
 });
 
+// Exporter le schéma
 module.exports = mongoose.model("Article", ArticleSchema);
