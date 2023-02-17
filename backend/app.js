244ed3fe5a0301b2a2c344072dd734e99d0745ee
noumedem tami florian <noumedemtamiflorian@gmail.com>
2023-02-17 17:11:59 +0100

    La configuration de la base de données

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,30 @@
+// Import des modules
 const express = require("express");
+const dotenv = require("dotenv");
 const ansicolor = require("ansi-colors");
+const db = require("./config/db.js");
 
+// Chargement des variables d'environnement
+dotenv.config();
+
+// Création de l'application
 const app = express();
 
+// Connexion à la base de données
+db();
+
+// Définition du port
 const port = process.env.PORT || 3000;
 
-// Démarrer le serveur
+// Exécution du serveur
 app.listen(port, () => {
     console.log(
         ansicolor.yellow.bold.italic("--------------------------------------")
     );
     console.log(
-        ansicolor.green.bold.italic(`Server is running on port ${port}`)
+        ansicolor.green.bold.italic(
+            `Serveur en cours d'exécution sur le port ${port}`
+        )
     );
     console.log(
         ansicolor.yellow.bold.italic("--------------------------------------")
