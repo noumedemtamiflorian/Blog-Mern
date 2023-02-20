04886bf6761e98dea3fdaad3d3dbbec3d8a3eb63
noumedem tami florian <noumedemtamiflorian@gmail.com>
2023-02-20 11:17:03 +0100

    Creation du Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,18 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 // Import the custom CSS file for the App component
 import "./App.css";
+// This line imports the Header component from the Header.js file in the components/common directory
+import Header from "./components/common/Header";
 
 // Define the App function component
 function App() {
     // Return a div with the className of "App" and the text "Init Frontend" as its child
-    return <div className="App">Init Frontend</div>;
+    return (
+        <>
+            <Header />
+            <div className="App">Init Frontend</div>
+        </>
+    );
 }
 
 // Export the App component as the default export of this module
