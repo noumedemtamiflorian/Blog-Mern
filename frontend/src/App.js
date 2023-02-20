a5ad8f6b18dfed87bb45d3a0518fb9e375f182f0
noumedem tami florian <noumedemtamiflorian@gmail.com>
2023-02-20 10:27:09 +0100

    Initialize frontend
    This commit initializes the React frontend for the blog project. The `App` component has been created and the necessary dependencies, such as `bootstrap` and `react-router-dom`, have been installed. The `App` component renders a simple "Init Frontend" message for now. This is the first step in building out the frontend of the blog application.

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// Import Bootstrap's CSS file and the custom CSS file for the App component
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+// Import the custom CSS file for the App component
+import "./App.css";
+
+// Define the App function component
+function App() {
+    // Return a div with the className of "App" and the text "Init Frontend" as its child
+    return <div className="App">Init Frontend</div>;
+}
+
+// Export the App component as the default export of this module
+export default App;
