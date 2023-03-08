0be200babe69e10a41b76cca43a0b6c955fbb244
noumedem tami florian <noumedemtamiflorian@gmail.com>
2023-03-08 13:25:56 +0100

    Creation de verifyToken pour verifier le token

---
+++ backend/config/jwt.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers["authorization"];
+
+    if (typeof bearerHeader !== "undefined") {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.status(403).json("Acces non autorise.");
+    }
+}
+
+module.exports = {
+    verifyToken: verifyToken,
+};
